fb3b5bd50c6c6ddd7e95005de81b4e64bb43ea14
eduardoBeserra <beserra.eduardo@gmail.com>
2019-04-07 18:37:33 -0300

    Importação de cargos, regime de trabalho, etnia e tempo de experiência.

--- import.js
+++ import.js
@@ -1,17 +1,35 @@
 const fs = require('fs')
 const path = require('path')
 const diretorio = './arquivos/'
+
 let importPerguntas = false
+let importCargos = false
+let importExp = false
+let importRegime = false
+let importEtnia = false
+
 let dbPerguntas = true
+let dbCargos = true
+let dbRegime = true
+let dbEtnia = true
+
 let perguntas = []
+let cargos = []
+let regimes = []
+let etnias = []
+
 let questionarios = []
+
+let idCargo = 0
+let idRegime = 0
+let idEtnia = 0
 
 class Questao {
     constructor(linha) {
         linha = linha.replace('"', '')
         let pergunta = linha.split(';')
         let splitLinha = pergunta[0].split('. ')
-        this.numero = splitLinha[0]
+        this.numeroj = splitLinha[0]
         this.questao = splitLinha[1]
         this.getResposta(linha)
     }
@@ -41,6 +59,54 @@ const importarPergunta = (questionario, linha) => {
         questionario.respostas.push({numero, resposta})
 }
 
+const importarCargos = (questionario, linha) => {
+    if(linha.match('Cargo;'))
+        return
+
+    idCargo++
+    if(dbCargos) {
+        descricao = linha.split(';')[0]
+        if(descricao)
+            cargos.push({ id: idCargo, descricao })
+    }
+    if(linha.split(';')[1].toLowerCase() === 'x')
+        questionario.cargo = idCargo
+}
+
+const importarExp = (questionario, linha) => {
+    let tempo = linha.split(';')[1]
+    if(tempo)
+        questionario.exp = tempo
+}
+
+const importarRegime = (questionario, linha) => {
+    if(linha.match('Regime de trabalho;'))
+        return
+
+    idRegime++
+    if(dbRegime) {
+        descricao = linha.split(';')[0]
+        if(descricao)
+            regimes.push({ id: idRegime, descricao })
+    }
+    if(linha.split(';')[1].toLowerCase() === 'x')
+        questionario.regime = idRegime
+}
+
+const importarEtnia = (questionario, linha) => {
+    if(linha.match('Grupo étnico;'))
+        return
+
+    idEtnia++
+    if(dbEtnia) {
+        descricao = linha.split(';')[0]
+        if(descricao)
+            etnias.push({ id: idEtnia, descricao })
+    }
+    if(linha.split(';')[1].toLowerCase() === 'x')
+        questionario.etnia = idEtnia
+}
+
 const lerDiretorio = () => {
     files = fs.readdirSync(diretorio)
     return files.filter(file => {
@@ -65,11 +131,32 @@ const importar = arq => {
             importPerguntas = true
             return
         }
-        else if(linha.match('Cargo;'))
+        else if(linha.match('Cargo;')) {
             importPerguntas = false
+            importCargos = true
+        } else if(linha.match('Tempo de Experiencia;')) {
+            importCargos = false
+            importExp = true
+        } else if(linha.match('Regime de trabalho;')) {
+            importExp = false
+            importRegime = true
+        } else if(linha.match('Grupo étnico;')) {
+            importRegime = false
+            importEtnia = true
+        } else if(linha.match('trabalha neste hospital;')) {
+            importEtnia = false
+        }
 
         if(importPerguntas)
             importarPergunta(questionario, linha)
+        else if(importCargos)
+            importarCargos(questionario, linha)
+        else if(importExp)
+            importarExp(questionario, linha)
+        else if(importRegime)
+            importarRegime(questionario, linha)
+        else if(importEtnia)
+            importarEtnia(questionario, linha)
     })
     return  questionario
 }
@@ -77,8 +164,20 @@ const importar = arq => {
 arquivos = lerDiretorio()
 
 arquivos.forEach(arq => {
+    idCargo = 0
+    idRegime = 0
+    idEtnia = 0
+
     let questionario = importar(arq)
     questionarios.push(questionario)
     dbPerguntas = false
+    dbCargos = false
+    dbRegime = false
+    dbEtnia = false
 })
 
+questionarios.forEach( questionario => {
+    console.log(questionario.etnia)
+})
+
+console.log(etnias)
