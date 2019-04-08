72827f4ab077c1e747fac68f173ca4a09227dbf8
eduardoBeserra <beserra.eduardo@gmail.com>
2019-04-08 08:19:49 -0300

    Fim das importações, inicio da definição dos domínios.

--- import.js
+++ import.js
@@ -7,29 +7,37 @@ let importCargos = false
 let importExp = false
 let importRegime = false
 let importEtnia = false
+let importTempTrabalho = false
+let importIdade = false
+let importTurno = false
+let importSexo = false
 
 let dbPerguntas = true
 let dbCargos = true
 let dbRegime = true
 let dbEtnia = true
+let dbTurnos = true
 
 let perguntas = []
 let cargos = []
 let regimes = []
 let etnias = []
+let turnos = []
+let dominios = []
 
 let questionarios = []
 
 let idCargo = 0
 let idRegime = 0
 let idEtnia = 0
+let idTurno = 0
 
 class Questao {
     constructor(linha) {
         linha = linha.replace('"', '')
         let pergunta = linha.split(';')
         let splitLinha = pergunta[0].split('. ')
-        this.numeroj = splitLinha[0]
+        this.numero = splitLinha[0]
         this.questao = splitLinha[1]
         this.getResposta(linha)
     }
@@ -107,6 +115,40 @@ const importarEtnia = (questionario, linha) => {
         questionario.etnia = idEtnia
 }
 
+const importarTempoTrabalho = (questionario, linha) => {
+    tempo = linha.split(';')[1]
+    if(tempo)
+        questionario.tempoTrabalho = tempo
+}
+
+const importarIdade = (questionario, linha) => {
+    idade = linha.split(';')[1]
+    if(idade)
+        questionario.idade = idade
+}
+
+const importarTurno = (questionario, linha) => {
+    if(linha.match('Turno Habitual;'))
+        return
+    
+    idTurno++
+    if(dbTurnos) {
+        descricao = linha.split(';')[0]
+        if(descricao)
+            turnos.push({ id: idTurno, descricao })
+    }
+    if(linha.split(';')[1].toLowerCase() === 'x')
+        questionario.turno = idTurno
+}
+
+const importarSexo = (questionario, linha) => {
+    if(linha.match('Sexo;'))
+        return
+
+    if(linha.split(';')[1] && linha.split(';')[1].toLowerCase() === 'x')
+        questionario.sexo = linha.split(';')[0]
+}
+
 const lerDiretorio = () => {
     files = fs.readdirSync(diretorio)
     return files.filter(file => {
@@ -125,6 +167,7 @@ const importar = arq => {
 
     linhas = texto.split('\n')
     let questionario = novoQuestionario()
+    questionario.arquivo = arq
 
     linhas.forEach(linha => {
         if(linha.match(';DT;DP')) {
@@ -145,6 +188,16 @@ const importar = arq => {
             importEtnia = true
         } else if(linha.match('trabalha neste hospital;')) {
             importEtnia = false
+            importTempTrabalho = true
+        } else if(linha.match('Idade;')) {
+            importTempTrabalho = false
+            importIdade = true
+        } else if(linha.match('Turno Habitual;')) {
+            importIdade = false
+            importTurno = true
+        } else if(linha.match('Sexo;')) {
+            importTurno = false
+            importSexo = true
         }
 
         if(importPerguntas)
@@ -157,8 +210,102 @@ const importar = arq => {
             importarRegime(questionario, linha)
         else if(importEtnia)
             importarEtnia(questionario, linha)
+        else if(importTempTrabalho)
+            importarTempoTrabalho(questionario, linha)
+        else if(importIdade)
+            importarIdade(questionario, linha)
+        else if(importTurno)
+            importarTurno(questionario, linha)
+        else if(importSexo)
+            importarSexo(questionario, linha)
     })
     return  questionario
+}
+
+const getPergunta = numero => {
+    return perguntas.filter(pergunta => {
+        return pergunta.numero == numero
+    })
+}
+
+const setDominios = () => {
+    dominios = [{
+        id: 1,
+        descricao: 'Clima de segurança',
+        qtdPerguntas: 7
+    }, {
+        id: 2,
+        descricao: 'Percepção da gerência',
+        qtdPerguntas: 5
+    }, {
+        id: 3,
+        descricao: 'Percepção do estresse',
+        qtdPerguntas: 4
+    }, {
+        id: 4,
+        descricao: 'Condição do Trabalho',
+        qtdPerguntas: 6
+    }, {
+        id: 5,
+        descricao: 'Comunicação no ambiente Cirúrgico',
+        qtdPerguntas: 4
+    }, {
+        id: 6,
+        descricao: 'Percepção do desempenho profissional',
+        qtdPerguntas: 4
+    }, {
+        id: 7,
+        descricao: 'Fator 7',
+        qtdPerguntas: 3
+    }, {
+        id: 8,
+        descricao: 'Fator 8',
+        qtdPerguntas: 1
+    }, {
+        id: 9,
+        descricao: 'Fator 9',
+        qtdPerguntas: 2
+    }, {
+        id: 10,
+        descricao: 'Fator 10',
+        qtdPerguntas: 2
+    }, {
+        id: 11,
+        descricao: 'Fator 11',
+        qtdPerguntas: 1
+    }, {
+        id: 12,
+        descricao: 'Fator 12',
+        qtdPerguntas: 1
+    }]
+
+    perguntas[27].dominio = 1
+    perguntas[28].dominio = 1
+    perguntas[29].dominio = 1
+    perguntas[31].dominio = 1
+    perguntas[33].dominio = 1
+    perguntas[34].dominio = 1
+    perguntas[37].dominio = 1
+
+    perguntas[4].dominio = 2
+    perguntas[5].dominio = 2
+    perguntas[6].dominio = 2
+    perguntas[10].dominio = 2
+    perguntas[13].dominio = 2
+
+    perguntas[30].dominio = 3
+    perguntas[32].dominio = 3
+    perguntas[35].dominio = 3
+    perguntas[36].dominio = 3
+
+    perguntas[14].dominio = 4
+    perguntas[16].dominio = 4
+    perguntas[17].dominio = 4
+    perguntas[21].dominio = 4
+    perguntas[22].dominio = 4
+    perguntas[25].dominio = 4
+
+    console.log(perguntas)
 }
 
 arquivos = lerDiretorio()
@@ -167,6 +314,7 @@ arquivos.forEach(arq => {
     idCargo = 0
     idRegime = 0
     idEtnia = 0
+    idTurno = 0
 
     let questionario = importar(arq)
     questionarios.push(questionario)
@@ -174,10 +322,9 @@ arquivos.forEach(arq => {
     dbCargos = false
     dbRegime = false
     dbEtnia = false
+    dbTurnos = false
+
+    importSexo = false
 })
 
-questionarios.forEach( questionario => {
-    console.log(questionario.etnia)
-})
-
-console.log(etnias)
+setDominios()
